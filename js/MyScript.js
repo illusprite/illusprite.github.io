b3cfe3607d11f857ee8af27f69378f49553fc49f
illusprite <90152987+illusprite@users.noreply.github.com>
2021-11-23 21:54:33 +0500

    Add files via upload

---
+++ js/MyScript.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*Тип
+Дизайн
+Адаптивность*/
+
+let Type = prompt("Какой тип сайта нужен Вам");
+console.log(Type);
+let Design = prompt("Какой дизайн Вы бы хотели видет на сайте?");
+console.log(Design);
+let Adapt = prompt("Под какие устройства нужно адаптировать сайт (Телефоны, ПК...)?");
+console.log(Adapt);
