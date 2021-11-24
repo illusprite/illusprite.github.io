4e8ae096eda7d269ff7a3d7d0c5aa52e78d9c9ed
illusprite <90152987+illusprite@users.noreply.github.com>
2021-11-24 20:33:42 +0500

    Add files via upload

--- js/MyScript.js
+++ js/MyScript.js
@@ -6,7 +6,17 @@
 
 let Type = prompt("Какой тип сайта нужен Вам");
 console.log(Type);
+let Type$=5000;
 let Design = prompt("Какой дизайн Вы бы хотели видет на сайте?");
 console.log(Design);
+let Design$=5000;
 let Adapt = prompt("Под какие устройства нужно адаптировать сайт (Телефоны, ПК...)?");
 console.log(Adapt);
+let Adapt$=5000;
+let inform = {
+	Type: Type,
+	Design: Design,
+	Adapt: Adapt,
+}
+console.log(inform);
+console.log(Type$+Design$+Adapt$);
